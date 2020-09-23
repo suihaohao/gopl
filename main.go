fe7f76a2618098ccc77c365205d916de067baa0a
suihaohao <1255151021@qq.com>
2020-09-23 18:11:31 +0800

    update reverse

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"gopl/reverse"
+)
+
 func main() {
 	//lissajous.RunLissajous()
 	//fetch_url.RunFetch()
 	//web_service.RunWebService()
+	fmt.Println(reverse.ReverseArr(&[]interface{}{1,2,3,4, "a", "b","c"}))
 }
--- reverse/reverse.go
+++ reverse/reverse.go
@@ -3,15 +3,18 @@ package reverse
 
 import "strings"
 
-func ReverseArr(s []interface{}) []interface{}{
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+func ReverseArr(s *[]interface{}) []interface{} {
+	for i := 0; i < len(*s)/2; i++ {
+		(*s)[i], (*s)[len(*s)-i-1] = (*s)[len(*s)-i-1], (*s)[i]
 	}
-	return s
+	//for i, j := 0, len(*s)-1; i < j; i, j = i+1, j-1 {
+	//	(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
+	//}
+	return *s
 }
 
-func ReverseString(s string) string{
-	sArr :=strings.Split(s, "")
+func ReverseString(s string) string {
+	sArr := strings.Split(s, "")
 	for i, j := 0, len(sArr)-1; i < j; i, j = i+1, j-1 {
 		sArr[i], sArr[j] = sArr[j], sArr[i]
 	}
